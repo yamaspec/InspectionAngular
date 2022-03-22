f1147b0fabec8bd3331263f3bef75594e0d67a0a
Yama <yama.spec@yahoo.es>
2022-03-22 13:32:21 +1100

    Basic CRUD done

--- src/app/inspection/add-edit-inspection/add-edit-inspection.component.ts
+++ src/app/inspection/add-edit-inspection/add-edit-inspection.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
 
 @Component({
   selector: 'app-add-edit-inspection',
@@ -7,9 +9,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditInspectionComponent implements OnInit {
 
-  constructor() { }
+  @Input() inspection:any;
+  inspectionList$!:Observable<any[]>;
+  inspectionTypeList$!:Observable<any[]>;
+  statusList$!:Observable<any[]>;
+
+  id:number=0;
+  status:string="";
+  comments:string="";
+  inspectionTypeId!:number;
+  // inspectionName!:string;
+
+  constructor(private service:InspectionApiService) { }
+
 
   ngOnInit(): void {
+    this.id               = this.inspection.id;
+    this.status           = this.inspection.status;
+    this.comments         = this.inspection.comments;
+    this.inspectionTypeId = this.inspection.inspectionTypeId;
+    // this.inspectionName   = this.inspectionName;
+
+    this.inspectionList$      = this.service.getInspectionList();
+    this.inspectionTypeList$  = this.service.getInspectionTypeList();
+    this.statusList$          = this.service.getStatusList();
   }
+
+  addInspection() {
+    var inspection = {
+      status:this.status,
+      comments:this.comments,
+      inspectionTypeId:this.inspectionTypeId
+    }
+      // Close component if save all changes successfully.
+    this.service.addInspection(inspection).subscribe(success => {
+        var closeMOdalButton = document.getElementById('add-edit-modal-close');
+      if(closeMOdalButton) {
+        closeMOdalButton.click();
+      }
+      var showAddSuccess = document.getElementById('add-success-alert');
+      if(showAddSuccess) {
+        showAddSuccess.style.display = "block";
+      }
+      setTimeout(function() {
+        if(showAddSuccess) {
+          showAddSuccess.style.display = "none";
+        }
+      }, 4000);
+    });
+  }
+
+  updateInspection() {
+    var inspection = {
+      id:this.id,
+      status:this.status,
+      comments:this.comments,
+      inspectionTypeId:this.inspectionTypeId
+    }
+    var id:number = this.id;
+    this.service.updateInspection(id, inspection).subscribe(success => {
+      // Close component if save all changes successfully.
+      var closeMOdalButton = document.getElementById('update-edit-modal-close');
+      if(closeMOdalButton) {
+        closeMOdalButton.click();
+      }
+      var showUpdateSuccess = document.getElementById('update-success-alert');
+      if(showUpdateSuccess) {
+        showUpdateSuccess.style.display = "block";
+      }
+      setTimeout(function() {
+        if(showUpdateSuccess) {
+          showUpdateSuccess.style.display = "none";
+        }
+      }, 4000);
+    });
+  }
+
+
 
 }
--- src/app/inspection/show-inspection/show-inspection.component.ts
+++ src/app/inspection/show-inspection/show-inspection.component.ts
@@ -12,16 +12,70 @@ export class ShowInspectionComponent implements OnInit {
 
   inspectionList$!:Observable<any[]>;
   inspectionTypeList$!:Observable<any[]>;
-  inspectionTypeList:any=[];
+  inspectionTypeList:any = [];
+  modalTitle:string = '';
+  activateAddEditInspectionComponent:boolean = false;
+  inspection:any;
 
   // Map to display data associated with FKs
-  inspectionTypeMap:Map<number, string> = new Map();
+  inspectionTypesMap:Map<number, string> = new Map();
 
   constructor(private service:InspectionApiService) { }
 
   ngOnInit(): void {
-    this.inspectionList$ = this.service.getInspectionList();
+    this.inspectionList$     = this.service.getInspectionList();
     this.inspectionTypeList$ = this.service.getInspectionTypeList();
+    this.refreshInspectionTypesMap();
+  }
+
+  refreshInspectionTypesMap() {
+    this.service.getInspectionTypeList().subscribe(data => {
+      this.inspectionTypeList = data;
+
+      for(let i = 0; this.inspectionTypeList.length; i++)
+      {
+        this.inspectionTypesMap.set(this.inspectionTypeList[i].id, this.inspectionTypeList[i].inspectionName);
+      }
+    });
+  }
+
+  modalAdd() {
+    this.inspection = {
+      id:0,
+      status:null,
+      comments:null,
+      inspectionTypeId:null
+    }
+    this.modalTitle = "Add a new Inspection";
+    this.activateAddEditInspectionComponent = true;
+  }
+
+  modalUpdate(updateInspection:any) {
+    this.inspection = updateInspection;
+    this.modalTitle = "Update an Inspection";
+    this.activateAddEditInspectionComponent = true;
+  }
+
+  deleteInspection(deleteInspection:any) {
+    if(confirm(`Please confirm to delete inspection: ${this.inspectionTypesMap.get(deleteInspection.inspectionTypeId)} - ${deleteInspection.status}`)) {
+      this.service.deleteInspection(deleteInspection.id).subscribe(success => {
+        var showDeleteSuccess = document.getElementById('delete-success-alert');
+        if(showDeleteSuccess) {
+          showDeleteSuccess.style.display = "block";
+        }
+        setTimeout(function() {
+          if(showDeleteSuccess) {
+            showDeleteSuccess.style.display = "none";
+          }
+        }, 4000);
+        this.inspectionList$ = this.service.getInspectionList();
+      })
+    }
+  }
+
+  modalClose() {
+    this.activateAddEditInspectionComponent = false;
+    this.inspectionList$ = this.service.getInspectionList();
   }
 
 }
